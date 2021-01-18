e0506d600bac252c2a2bb87cb0298d827a7d157b
Your Name <you@example.com>
2021-01-18 02:34:04 +0000

    init

---
+++ src/main2.js
@@ -0,0 +1,13 @@
+main();
+
+function main() {
+    // console.log("hello world")
+    const a = 1;
+    const b = 2;
+    const result = sum(a, b);
+    console.log(a, "+", b, "の合計は", result, "です。");
+}
+
+function sum(a, b) {
+    return a + b;
+}
